93d03bb779d38623fa0350b19a70010227d691d4
sensoyyasin <hallowiertndio@gmail.com>
2021-07-23 18:18:25 +0300

    SpaceRocket

---
+++ android/src/com/sensoyyasin/spacerocket/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.sensoyyasin.spacerocket;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.sensoyyasin.spacerocket.SpaceRocket;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SpaceRocket(), config);
+	}
+}
